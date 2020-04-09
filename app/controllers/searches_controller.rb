fabbe1696bb86e6d2a5488eef58f8a6bc147a334
Genki-1 <60809881+Genki-1@users.noreply.github.com>
2020-04-09 19:03:02 +0900

    [Finish] bookers2_kadai5

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,14 +5,17 @@ class SearchesController < ApplicationController
   # GET /searches.json
   def index
     #ViewのFormで取得したパラメータをモデルに渡す
-    @user_searches = Search.user_search(params[:search])
-    @book_searches = Search.book_search(params[:search])
+    #@user_searches = Search.user_search(params[:search])
+    #@book_searches = Search.book_search(params[:search])
+    @model = params["search"]["model"]
+    @content = params["search"]["content"]
+    @how = params["search"]["how"]
+    @records = search_for(@how, @model, @content)
   end
-
 
   def search
-    @user_searches = User.search(params[:search])
   end
+
 
   # GET /searches/1
   # GET /searches/1.json
@@ -35,4 +38,50 @@ class SearchesController < ApplicationController
     def search_params
       params.require(:search).permit(:name)
     end
+
+  def match(model, content)
+    if model == 'user'
+      User.where(name: content)
+    elsif model == 'book'
+      Book.where(title: content)
+    end
+  end
+
+  def forward(model, content)
+    if model == 'user'
+      User.where("name LIKE ?", "#{content}%")
+    elsif model == 'book'
+      Book.where("title LIKE ?", "#{content}%")
+    end
+  end
+
+  def backward(model, content)
+    if model == 'user'
+      User.where("name LIKE ?", "%#{content}")
+    elsif model == 'book'
+      Book.where("title LIKE ?", "%#{content}")
+    end
+  end
+
+  def partical(model, content)
+    if model == 'user'
+      User.where("name LIKE ?", "%#{content}%")
+    elsif model == 'book'
+      Book.where("title LIKE ?", "%#{content}%")
+    end
+  end
+
+  def search_for(how, model, content)
+    case how
+    when 'match'
+      match(model, content)
+    when 'forward'
+      forward(model, content)
+    when 'backward'
+      backward(model, content)
+    when 'partical'
+      partical(model, content)
+    end
+  end
+
 end
