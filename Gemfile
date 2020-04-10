b00409fe15ceb8e81dee3404c21ec87e0b871ee4
Genki-1 <60809881+Genki-1@users.noreply.github.com>
2020-04-10 21:25:58 +0900

    [Add] bookers2_kadai6

--- Gemfile
+++ Gemfile
@@ -63,6 +63,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 #originl
 gem 'bootstrap-sass', '~> 3.3.6'
+
 gem 'jquery-rails'
 
 gem 'devise'
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,12 @@
 class Favorite < ApplicationRecord
 	belongs_to :user
   	belongs_to :book
+
+  	def favorited_by?(user)
+       favorites.where(user_id: user.id).exists?
+    end
+
+	def books
+	  return Book.find_by(user_id: self.id)
+	end
 end
